235e4f5282c0864a329c0431538dcc08a0dd5140
gozdealtunbulak <72446777+gozdealtunbulak@users.noreply.github.com>
2020-10-20 14:46:48 +0300

    Update main.py
    these changes make my original code work faster

--- main.py
+++ main.py
@@ -2,20 +2,24 @@ import openpyxl
 import xlrd
 from openpyxl import Workbook, load_workbook
 import pandas as pd
+import time
 
-name = input('Name of the excel file: ')
-extension = input('The extension: ')
-path = input('The location of the file: ')
+name = 'test2'
+extension = 'xlsx'
+path = r'C:\Users\GOZDE\Desktop\excel'
 f_name = name + '.' + extension
+wb = xlrd.open_workbook('{}\{}'.format(path, f_name))
+wb1 = openpyxl.load_workbook('{}\{}'.format(path, f_name))
 
 
 def check_list(s_num, num):
     """
-    :param s_num: the sheet index
+    :param s_num:the sheet index
+    :type s_num int
     :param num: the row number
+    :type num int
     :return: this function returns a list of the desired row of the chosen sheet
     """
-    wb = xlrd.open_workbook(r'{}\{}'.format(path, f_name))
     sheet = wb.sheet_by_index(s_num)
     list_check = []
     row_check = sheet.row_values(num)
@@ -26,32 +30,35 @@ def check_list(s_num, num):
 def add_new_sheet(sheet_name, index):
     """
     :param index: index of the sheet
+    :type index int
     :param sheet_name: name of the sheet to be created
+    :type sheet_name str
     :return:create the sheet
     """
-    wb1 = load_workbook(r'{}\{}'.format(path, f_name))
     wb1.create_sheet(sheet_name, index=index)
-    wb1.save(r'{}\{}'.format(path, f_name))
+    wb1.save('{}\{}'.format(path, f_name))
 
 
 def remove_sheet(s_name):
     """"
     :param s_name of the sheet
+    :type s_name str
     :return: removes the desired sheet
     """
-    wb = openpyxl.load_workbook(r'{}\{}'.format(path, f_name))
-    sheet = wb.get_sheet_by_name(s_name)
-    wb.remove_sheet(sheet)
-    wb.save(r'{}\{}'.format(path, f_name))
+    wb3 = openpyxl.load_workbook('{}\{}'.format(path, f_name))
+    sheet = wb3[s_name]
+    wb3.remove(sheet)
+    wb3.save('{}\{}'.format(path, f_name))
 
 
 def read_row_num(s_name):
     """
     :param s_name: name of the sheet
+    :type s_name str
     :return:reads the row number of the sheet desired
     """
-    wb = xlrd.open_workbook(r'{}\{}'.format(path, f_name))
-    wk = wb.sheet_by_name(s_name)
+    wb2 = xlrd.open_workbook('{}\{}'.format(path, f_name))
+    wk = wb2.sheet_by_name(s_name)
     existing_row = wk.nrows
     return existing_row
 
@@ -59,11 +66,12 @@ def read_row_num(s_name):
 def add_data(s_name, data):
     """
     :param data: data to be added
+    :type s_name str
     :param s_name: name of the sheet
+    :type data list
     :return add the desired data to the desired sheet from the last row
     """
-    wbk = openpyxl.load_workbook(r'{}\{}'.format(path, f_name))
-    wks = wbk[s_name]
+    wks = wb1[s_name]
     starting_row = 1 + read_row_num(s_name)
     starting_col = 1
 
@@ -71,17 +79,19 @@ def add_data(s_name, data):
         for col, val in enumerate(item):
             wks.cell(row=row + starting_row, column=col + starting_col).value = val
 
-    wbk.save(r'{}\{}'.format(path, f_name))
-    wbk.close()
+    wb1.save('{}\{}'.format(path, f_name))
+    wb1.close()
 
 
 def drop_dub(s_index, keep):
     """
     :param s_index: index number of the sheet to be worked on
+    :type s_index int
     :param keep: keep type of the drop_dublicates function
+    :type keep str
     :return: drops the dublicate rows of the chosen sheet
     """
-    path2 = r'{}\{}'.format(path, f_name)
+    path2 = '{}\{}'.format(path, f_name)
     df = pd.read_excel(r'{}\{}'.format(path, f_name), sheet_name=s_index, index_col=None)
     df.drop_duplicates(keep=keep, inplace=True)
 
@@ -96,18 +106,22 @@ def drop_dub(s_index, keep):
 def compare(s_base, s_check, turn):
     """
     :param turn:number of times compare function is used
+    :type turn int
     :param s_base: base sheet index
+    :type s_base int
     :param s_check: sheet index for the check sheet
+    :type s_check int
     :return:Given a base sheet, this function tries to get the blank cell information from the check sheet.
     If the check list also does not have the information then it leaves the cell blank
     """
-    wb = xlrd.open_workbook(r'{}\{}'.format(path, f_name))
     final_list = []
     base_sheet = wb.sheet_by_index(s_base)
+    count = 0
     for i in range(base_sheet.nrows):
         list1 = []
         row_value = base_sheet.row_values(i)
         list1.extend(row_value)
+        """print(count)"""
         if "" in list1:
             for x, x in enumerate(list1):
                 if x == "":
@@ -117,13 +131,27 @@ def compare(s_base, s_check, turn):
                     else:
                         pass
         final_list.append(list1)
+        count += 1
     if turn > 1:
         final_list.pop(0)
     return final_list
 
 
+tic = time.perf_counter()
 add_new_sheet('merge', 2)
+toc = time.perf_counter()
+print(f"time to add new sheet: {toc - tic:0.4f} seconds")
 add_data('merge', compare(0, 1, 1))
+toc2 = time.perf_counter()
+print(f"time to add new data1: {toc2 - toc:0.4f} seconds")
 add_data('merge', compare(1, 0, 2))
+toc3 = time.perf_counter()
+print(f"time to add new data2: {toc3 - toc2:0.4f} seconds")
 drop_dub(2, 'first')
+toc4 = time.perf_counter()
+print(f"time to drop dub: {toc4 - toc3:0.4f} seconds")
 remove_sheet('merge')
+toc5 = time.perf_counter()
+print(f"time to remove sheet: {toc5 - toc4:0.4f} seconds")
+print(f"total time: {toc5 - tic:0.4f} seconds")
+print('finished')
